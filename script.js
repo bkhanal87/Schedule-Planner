ee523b263a99ecb0756b7a1ecca0bd812ca51e61
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-08-23 20:59:01 -0400

    updated all files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 document.getElementById("currentDay").innerHTML = Date();
+
+$(document).ready(function(){
+    $(".saveBtn").on("click",function(){
+        let userDescription = $(this).siblings(".description").val()
+        let time = $(this).parent().attr("id")
+        console.log(userDescription,time);
+        localStorage.setItem(time, userDescription)
+    })
+$("#9 .description").val(localStorage.getItem("9"))
+}) 
+
