44b1ffa418f57ddfed634773762cc4aa79d43e99
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-08-27 22:56:04 -0400

    changes to all files

--- script.js
+++ script.js
@@ -8,5 +8,24 @@ $(document).ready(function(){
         localStorage.setItem(time, userDescription)
     })
 $("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
 }) 
+
+let now = moment();
+
+moment("08-27-2021", "MM-DD-YYYY hh:mm:ss", true);
+
+moment().hour(Number);
+
+
+
+
+
 
